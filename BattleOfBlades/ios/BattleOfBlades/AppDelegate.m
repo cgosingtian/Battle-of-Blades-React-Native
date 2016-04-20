02d7fff3488dbda9f966ccb48e008dbab28a3147
Chase Gosingtian <chase.gosingtian@gmail.com>
2016-04-20 17:32:58 +0800

    Added AppHub; used AppHub to display list of possible builds to load them live.

--- BattleOfBlades/ios/BattleOfBlades/AppDelegate.m
+++ BattleOfBlades/ios/BattleOfBlades/AppDelegate.m
@@ -8,14 +8,36 @@
  */
 
 #import "AppDelegate.h"
-
 #import "RCTRootView.h"
 
+#import "AppHub.h"
+#import "RCTBridge.h"
+#import "RCTJavaScriptLoader.h"
+
+@interface AppDelegate() <RCTBridgeDelegate, UIAlertViewDelegate>
+
+@property (nonatomic, assign) BOOL buildSelected;
+@property (nonatomic, strong) NSString* currentBuildIdentifier;
+
+@end
+
 @implementation AppDelegate
+{
+  RCTBridge *_bridge;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  NSURL *jsCodeLocation;
+  self.buildSelected = NO;
+  
+  [AppHub setApplicationID:@"AYJafx8Z73rK6517GpEI"];
+  [AppHub buildManager].automaticPollingEnabled = NO;
+  [AppHub buildManager].cellularDownloadsEnabled = YES;
+
+  _bridge = [[RCTBridge alloc] initWithDelegate:self
+                                  launchOptions:launchOptions];
+  
+//  NSURL *jsCodeLocation;
 
   /**
    * Loading JavaScript code - uncomment the one you want.
@@ -31,7 +53,7 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+//  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
@@ -42,18 +64,121 @@
    */
 
 //   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"BattleOfBlades"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
-
+  
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:_bridge
+                                                   moduleName:@"BattleOfBlades"
+                                            initialProperties:nil];
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  // Register a callback for when a new build becomes available.
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(newBuildDidBecomeAvailable:)
+                                               name:AHBuildManagerDidMakeBuildAvailableNotification
+                                             object:nil];
+  
+  [AppHub presentSelectorOnViewController:rootViewController withBuildHandler:^(AHBuild *build, NSError *error) {
+    if (error || ! build) {
+      // An error occurred.
+      UIAlertView *alert = [[UIAlertView alloc] initWithTitle:error.localizedFailureReason message:error.localizedDescription delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
+      [alert show];
+    } else {
+        RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:_bridge
+                                                         moduleName:@"BattleOfBlades"
+                                                  initialProperties:nil];
+        
+        UIViewController *rootViewController = [UIViewController new];
+        rootViewController.view = rootView;
+        [self.window.rootViewController presentViewController:rootViewController animated:NO completion:nil];
+    }
+  }];
+  
   return YES;
+}
+
+#pragma mark - RCTBridgeDelegate
+
+- (NSURL *)sourceURLForBridge:(__unused RCTBridge *)bridge
+{
+  NSURL *sourceURL;
+  
+  /**
+   * Loading JavaScript code - uncomment the one you want.
+   *
+   * OPTION 1
+   * Load from development server. Start the server from the repository root:
+   *
+   * $ react-native start
+   *
+   * To run on device, change `localhost` to the IP address of your computer
+   * (you can get this by typing `ifconfig` into the terminal and selecting the
+   * `inet` value under `en0:`) and make sure your computer and iOS device are
+   * on the same Wi-Fi network.
+   */
+  
+  //  sourceURL = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  
+  /**
+   * OPTION 2 - AppHub
+   *
+   * Load cached code and images from AppHub. Use this when deploying to test
+   * users and the App Store.
+   *
+   * Make sure to re-generate the static bundle by navigating to your Xcode project
+   * folder and running
+   *
+   * $ react-native bundle --entry-file index.ios.js --platform ios --dev true --bundle-output iOS/main.jsbundle
+   *
+   */
+  
+  AHBuild *build = [[AppHub buildManager] currentBuild];
+  self.currentBuildIdentifier = build.identifier;
+  sourceURL = [build.bundle URLForResource:@"main"
+                             withExtension:@"jsbundle"];
+  
+  return sourceURL;
+}
+
+- (void)loadSourceForBridge:(RCTBridge *)bridge
+                  withBlock:(RCTSourceLoadBlock)loadCallback
+{
+  [RCTJavaScriptLoader loadBundleAtURL:[self sourceURLForBridge:bridge]
+                            onComplete:loadCallback];
+}
+
+
+#pragma mark - NSNotificationCenter
+
+-(void) newBuildDidBecomeAvailable:(NSNotification *)notification {
+  // Show an alert view when a new build becomes available. The user can choose to "Update" the app, or "Cancel".
+  // If the user presses "Cancel", their app will update when they close the app.
+  
+  AHBuild *build = notification.userInfo[AHBuildManagerBuildKey];
+  NSString *alertMessage = [NSString stringWithFormat:@"There's a new update available.\n\nUpdate description:\n\n %@", build.buildDescription];
+  
+  UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Great news!"
+                                                  message:alertMessage
+                                                 delegate:self
+                                        cancelButtonTitle:@"Cancel"
+                                        otherButtonTitles:@"Update", nil];
+  
+  dispatch_async(dispatch_get_main_queue(), ^{
+    // Show the alert on the main thread.
+    [alert show];
+  });
+}
+
+#pragma mark - UIAlertViewDelegate
+
+-(void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+  if (buttonIndex == 1) {
+    // The user pressed "update".
+    [_bridge reload];
+  }
 }
 
 @end
